d6d4c97892f2a2716e467bf487b76eec01b9ea2f
Angel Baro <angelbaro44@gmail.com>
2020-04-09 17:45:00 -0400

    test

--- js/index.js
+++ js/index.js
@@ -1,7 +1,104 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
 
-  function startGame() {}
-};
+var ballRadius = canvas.width / 20;
+
+var dx = 2;
+var dy = -2;
+// paddle
+var paddleHeight = 10;
+var paddleWidth = 75 + ballRadius;
+var paddleX = (canvas.width-paddleWidth)/2;
+// .keymapping
+var rightPressed = false;
+var leftPressed = false;
+var img = new Image();   // Create new img element
+img.src = 'images/car.png'; // Set source path
+var imgReducer = -20;
+
+// var bricks = [];
+// for(var c=0; c<brickColumnCount; c++) {
+//     bricks[c] = [];
+//     for(var r=0; r<brickRowCount; r++) {
+//         bricks[c][r] = { x: 0, y: 0 };
+//     }
+// }
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.rect(x,y,ballRadius,ballRadius,0)
+    ctx.fillStyle = img;
+    ctx.fill();
+    ctx.closePath();
+}
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    drawPaddle();
+    console.log(x+dx'x')
+    console.log(y+dy"y")
+
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy < ballRadius) {
+        dy = -dy;
+    }
+    else if(y + dy > canvas.height-ballRadius) {
+        if(x > (paddleX - 5) && x < (paddleX + 5 +paddleWidth)) {
+           if(y = y-paddleHeight){
+            dy = -dy  ;
+            
+			 }
+        }
+        else {
+            // alert("GAME OVER");
+            document.location.reload();
+            clearInterval(interval); // Needed for Chrome to end game
+        }
+    }
+    
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 8;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 8;
+    }
+    
+    x += dx;
+    y += dy;
+}
+var speed = 10;
+var interval = setInterval(draw, 1);

b64a899fc0f682961f029ae3f7fad8f8a588aa9c
Angel Baro <angelbaro44@gmail.com>
2020-04-10 01:00:17 -0400

    test

--- js/index.js
+++ js/index.js
@@ -1,78 +1,134 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var ballRadius = 10;
+var ballRadius = canvas.width/30;
+var paddleHeight = canvas.height/6;
+var paddleWidth = canvas.width/8;
+var paddleX = (canvas.width-paddleWidth)/2;
 var x = canvas.width/2;
-var y = canvas.height-30;
+var y = Math.floor((Math.random() * 50) + paddleHeight + 5);
 var dx = 2;
 var dy = -2;
-var paddleHeight = 10;
-var paddleWidth = 75;
-var paddleX = (canvas.width-paddleWidth)/2;
+
 var rightPressed = false;
 var leftPressed = false;
+var score = 10;
+
+var loveball = new Image();   // Create new img element
+loveball.src = 'images/pokemon love 2.png'; // Set source path
+
+var rioluLeft = new Image();   // Create new img element
+rioluLeft.src = 'images/riolu left.png'; // Set source path
+
+var rioluRight = new Image();   // Create new img element
+rioluRight.src = 'images/riolu right.png'; // Set source path
+
+var rioluUp = new Image();   // Create new img element
+rioluUp.src = 'images/riolu up-right.png'; // Set source path
+
+var maylene = new Image();   // Create new img element
+maylene.src ='images/hooh.png'; // Set source path
+
+var canvasBack = new Image();   // Create new img element
+canvasBack.src = 'images/18oiwrk32uilpgif.gif'; // Set source path
+
+
+
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
 function keyDownHandler(e) {
+    if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
     if(e.key == "Right" || e.key == "ArrowRight") {
         rightPressed = true;
     }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = true;
-    }
+  
 }
 
 function keyUpHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = false;
-    }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
+    if(e.key == "Left" || e.key == "ArrowLeft") {
         leftPressed = false;
     }
+     if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+   
 }
 
 function drawBall() {
+    
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#f";
+    // ctx.arc(x, (y), ballRadius, 0, Math.PI*2);
+    ctx.drawImage(loveball, x, y,ballRadius,ballRadius)
+    ctx.fillStyle = loveball;
     ctx.fill();
     ctx.closePath();
 }
 function drawPaddle() {
     ctx.beginPath();
-    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+        //  ctx.clearRect(0, 0, canvas.width, canvas.height);
+       // ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    if(leftPressed === true){
+    //  ctx.clearRect(0, 0, canvas.width, canvas.height);
+     ctx.drawImage(rioluLeft, paddleX, canvas.height-paddleHeight,paddleWidth,paddleHeight);
+     ctx.fillStyle = rioluLeft;
+     ctx.fill();
+     ctx.closePath();
+     console.log('helo left')}
+  if(rightPressed === true){
+    //  ctx.clearRect(0, 0, canvas.width, canvas.height);
+     ctx.drawImage(rioluRight, paddleX, canvas.height-paddleHeight,paddleWidth,paddleHeight);
+     ctx.fillStyle = rioluRight;
+     ctx.fill();
+     ctx.closePath();
+     console.log('helo right')}
+   if(rightPressed === leftPressed){
+        // ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(rioluUp, paddleX, canvas.height-paddleHeight,paddleWidth,paddleHeight);
+        ctx.fillStyle = rioluUp;
+        ctx.fill();
+        ctx.closePath();
+        console.log('helo up') }
 }
 function drawPaddle2() {
     ctx.beginPath();
-    ctx.rect(x, 0, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
+    ctx.drawImage(maylene, x - 20, 0 ,paddleWidth + 100,paddleHeight + 20);
+    ctx.fillStyle = maylene;
     ctx.fill();
     ctx.closePath();
 }
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(canvasBack,0, 0, canvas.width, canvas.height)
     drawBall();
     drawPaddle();
     drawPaddle2(); 
+    document.querySelector("body > h1").innerText= score + ' Health '
+    console.log(y)
+    if(score === 0 ){
+        alert("GAME Won!!!!");
+        document.location.reload();
+        clearInterval(interval); // Needed for Chrome to end game
+    }
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-    if(y + dy < ballRadius) {
+    if(y + dy < paddleHeight) {
         dy = -dy;
     }
-    else if(y + dy > canvas.height-ballRadius) {
+    else if(y + dy > canvas.height-paddleHeight) {
         if(x > paddleX - ballRadius && x < paddleX + ballRadius + paddleWidth) {
             dx++;
             dy++;
-
+            score--;
+            console.log(score)
             dy = -dy;
+
         }
         else {
-            alert("GAME OVER");
+            // alert("GAME OVER");
             document.location.reload();
             clearInterval(interval); // Needed for Chrome to end game
         }
@@ -89,4 +145,4 @@ function draw() {
     y += dy;
 }
 
-var interval = setInterval(draw, 10);
+var interval = setInterval(draw, 15);
